138cf7a45b069c5e271aae26e3fc55f44b4eb927
Saad-04 <asaad4674@gmail.com>
2024-02-28 02:57:28 +0500

    update env port for production

--- build/middleware/catchAsyncError.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsyncError = void 0;
-const catchAsyncError = (theFunc) => (req, res, next) => {
-    Promise.resolve(theFunc(req, res, next)).catch(next);
-};
-exports.catchAsyncError = catchAsyncError;
